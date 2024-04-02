d1faf11654eb4714b5d8de9a8a54a829e2acdbe1
BOMMAKAPARANDHAMAREDDY <149357107+BOMMAKAPARANDHAMAREDDY@users.noreply.github.com>
2024-04-02 17:42:03 +0530

    completed ASAP Project

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,21 @@
 /* eslint-disable no-unused-vars */
+import React from "react";
 import "./App.css";
+import LoginOrLogOut from "./Form/LoginOrLogOut";
 import Entity from "./components/Entity";
-// import LandingPage from "./components/LandingPage";
+import LandingPage from "./components/LandingPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <>
-  {/* <LandingPage /> */}
-  <Entity />
-  </>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginOrLogOut />} />
+        <Route path="/entity" element={<Entity />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
